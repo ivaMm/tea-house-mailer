b0433fd33fa84c5b198477f2258e723680984f1b
iva mudronja <iva.pilizota@gmail.com>
2020-10-06 21:19:25 +0200

    added mailjet

--- app/mailers/tea_mailer.rb
+++ app/mailers/tea_mailer.rb
@@ -1,6 +1,12 @@
 class TeaMailer < ApplicationMailer
    def welcome_email(user)
     @user = user
-    mail(to: @user.email, subject: 'Hello from TeaHouse!')
+    mail(
+      from: "Tea House",
+      to: @user.email,
+      subject: 'Hello from TeaHouse!') do |format|
+        format.html { render 'welcome_email.html.erb' }
+        format.text { render plain: 'welcome_email.text.erb' }
+      end
   end
 end
