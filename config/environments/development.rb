3eea6f26518ceea0f63b8d005f10af0cb1a659b2
iva mudronja <iva.pilizota@gmail.com>
2021-07-04 23:25:08 +0200

    devise mailer confirm + change password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,9 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+  config.action_mailer.default_url_options = { host: "http://localhost:3000/" }
   config.action_mailer.delivery_method = :mailjet
   # Settings specified here will take precedence over those in config/application.rb.
+
+  config.action_mailer.asset_host = 'http://localhost:3000'
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
